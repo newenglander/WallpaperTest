ab21a9c0bce205e2d21217c0c313dea9ecc6f7dd
test <test>
2014-07-09 10:22:20 +0200

    Forgot to add necessary flags.

--- WallpaperTest/ViewController.m
+++ WallpaperTest/ViewController.m
@@ -24,16 +24,10 @@
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
-    //self = [self initWithUIImage:[UIImage imageWithContentsOfFile:@"/System/Library/WidgetResources/ibutton/white_i@2x.png"]];
-
-CGRect aRect = CGRectMake(0,0,0,0);
-    // - (id)initWithUIImage:(id)arg1 cropRect:(struct CGRect { struct CGPoint { float x_1_1_1; float x_1_1_2; } x1; struct CGSize { float x_2_1_1; float x_2_1_2; } x2; })arg2;
-    struct CGRect { struct CGPoint { float x_1_1_1; float x_1_1_2; } x1; struct CGSize { float x_2_1_1; float x_2_1_2; } x2;}arg2;
-    
-    
+    // black_i
     self = [self initWithUIImage:[UIImage imageWithContentsOfFile:@"/System/Library/WidgetResources/ibutton/white_i@2x.png"]];
     self.allowsEditing = YES;
-    //self = [self initWithUIImage:[UIImage imageWithContentsOfFile:@"/System/Library/WidgetResources/ibutton/white_i@2x.png"] cropRect:arg2];
+    self.saveWallpaperData = YES;
 
     return self;
 }
