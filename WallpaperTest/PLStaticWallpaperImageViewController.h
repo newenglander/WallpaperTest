9efc15067dd667505973df35358c4849cf486239
test <test>
2014-07-21 15:29:13 +0200

    remove unused line

--- WallpaperTest/PLStaticWallpaperImageViewController.h
+++ WallpaperTest/PLStaticWallpaperImageViewController.h
@@ -13,7 +13,7 @@
 - (id)wallpaperImage;
 - (void)setColorSamplingEnabled:(BOOL)arg1;
 - (BOOL)colorSamplingEnabled;
-- (id)initWithUIImage:(id)arg1 cropRect:(struct CGRect { struct CGPoint { float x_1_1_1; float x_1_1_2; } x1; struct CGSize { float x_2_1_1; float x_2_1_2; } x2; })arg2;
+// - (id)initWithUIImage:(id)arg1 cropRect:(struct CGRect { struct CGPoint { float x_1_1_1; float x_1_1_2; } x1; struct CGSize { float x_2_1_1; float x_2_1_2; } x2; })arg2;
 - (void)viewWillAppear:(BOOL)arg1;
 
 
